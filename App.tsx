9e8db10a82b93fbd5016f1071903b8e22e8c2ca1
Jack Gumoes <j.gumoes@gmail.com>
2023-09-04 17:48:40 +0100

    fixed the App render test the app actually renders the game now lol

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { ErrorBoundary } from "react-error-boundary";
 import { SafeAreaView } from 'react-native';
 import Grid, { colorTestGrid } from './src/grid';
 import React, { useState } from 'react';
@@ -6,15 +7,33 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { appStyles } from './AppStyles';
 import GameBoard from './src/GameBoard';
 
+const swipeForGameWonGrid = () => [
+  [ 0, 0, 0, 0], 
+  [ 0, 1024, 1024, 0],
+  [ 0, 1024, 1024, 0], 
+  [ 0, 0, 0, 0], 
+]
 export default function App() {
   // const [grid, setGrid] = useState(new Grid({gridSize: 4}))
-  const [grid, setGrid] = useState(new Grid({grid: colorTestGrid(), undoCount: 5}))
+  // const [grid, setGrid] = useState(new Grid({grid: colorTestGrid(), undoCount: 5}))
+  const [grid, setGrid] = useState(() => {
+    const newGrid = new Grid({grid: swipeForGameWonGrid()})
+    // const newGrid = new Grid({grid: colorTestGrid(), score: 42069, undoCount: 5})
+    // const newGrid = new Grid({grid: [
+    //   [ 8, 8, 4, 2], 
+    //   [ 4, 4, 2, 4], 
+    //   [ 2, 2, 4, 2], 
+    //   [ 0, 4, 2, 4]
+    // ]})
+    // GridOverlayState.primeGameWon = newGrid.isGameWon
+    return newGrid
+  })
   
   return (
     <SafeAreaView style={[appStyles.container]}>
-        <StatusBar style="auto" hidden={false} />
+      <StatusBar style="auto" hidden={false} />
       <GestureHandlerRootView style={[appStyles.container, {marginTop: 80}]}>
-        <GameBoard grid={grid} />
+        <GameBoard grid={grid}/>
       </GestureHandlerRootView>
     </SafeAreaView>
   );
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,5 +1,17 @@
 import { render, screen } from "@testing-library/react-native"
 import App from "../App"
+import { StyleProp, ViewStyle } from "react-native"
+
+jest.mock('../src/SVGIcons', () => {
+  return{
+    ResetSquare: () => {
+      return <view />
+    },
+    UndoIcon: () => {
+      return <view />
+    }
+  }
+})
 
 jest.mock('expo-status-bar', ()=>{
   return({
@@ -11,14 +23,28 @@ jest.mock('expo-status-bar', ()=>{
 
 jest.mock('react-native-gesture-handler', ()=>{
   return({
-    GestureHandlerRootView: () => {
-      <view />
+    GestureHandlerRootView: ({children, style}: {children: JSX.Element, style: StyleProp<ViewStyle>}) => {
+      return(<view>{children}</view>)
     },
-    gestureHandlerRootHOC: (element: JSX.Element) => element
+    gestureHandlerRootHOC: (element: JSX.Element) => element,
+    Gesture: {
+      Pan: jest.fn().mockImplementation(() => {
+        return new class {
+          minDistance = jest.fn().mockReturnThis()
+          maxPointers = jest.fn().mockReturnThis()
+          onBegin = jest.fn().mockReturnThis()
+          onStart = jest.fn().mockReturnThis()
+        }
+      })
+    },
+    GestureDetector: ({children}: {children: JSX.Element}) => {
+      return(<view>{children}</view>)
+    }
   })
 })
 
 test('App renders', () => {
+  console.log(<App />)
   render(<App />)
   expect(screen.toJSON()).not.toBeNull()
   expect(screen).toMatchSnapshot()
